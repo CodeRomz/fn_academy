2220c98ebb2801e0e1fa6804867af96bb2fa7178
CyberRoms <romualdojrb@yahoo.com>
2024-10-11 16:24:57 +0400

    .

--- models/survey_survey.py
+++ models/survey_survey.py
@@ -6,6 +6,6 @@ class SurveySurvey(models.Model):
     # Extend the certification_report_layout selection field to add 'firenor_v1'
     certification_report_layout = fields.Selection(
         selection_add=[
-            ('modern_seagreen', 'Firenor V1')
+            ('firenor_seagreen', 'Firenor Seagreen')
         ],
     )
