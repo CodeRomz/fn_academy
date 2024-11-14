27783c741e5a8b3385ad2933bd6b63e51d860bbc
CyberRoms <romualdojrb@yahoo.com>
2024-11-14 13:02:14 +0400

    .

--- controllers/certification_web_view.py
+++ controllers/certification_web_view.py
@@ -1,31 +1,17 @@
 from odoo import http
 from odoo.http import request
 
-# class SurveyCertificateController(http.Controller):
-#     @http.route(['/fna/certification/<string:public_access_token>'], type='http', auth='public', website=True)
-#     def view_certificate(self, public_access_token, **kwargs):
-#         # Find the user input with the matching public access token
-#         user_input = request.env['survey.user_input'].sudo().search([('public_access_token', '=', public_access_token)], limit=1)
-#         if not user_input:
-#             return request.not_found()
-#
-#         values = {
-#             'doc': user_input,
-#             'share_url': request.httprequest.host_url + 'fna/certification/' + public_access_token
-#         }
-#         return request.render('survey.certification_report_view', values)
-
-
 class SurveyCertificateController(http.Controller):
-    @http.route(['/fna/certification/<string:access_token>'], type='http', auth='public', website=True)
-    def view_certificate(self, access_token, **kwargs):
-        # Find the user input with the matching access token
-        user_input = request.env['survey.user_input'].sudo().search([('access_token', '=', access_token)], limit=1)
-        if not user_input:
+    @http.route(['/fna/certification/<int:user_input_id>'], type='http', auth='public', website=True)
+    def view_certificate(self, user_input_id, **kwargs):
+        # Fetch the survey response using sudo() to bypass security restrictions
+        user_input = request.env['survey.user_input'].sudo().browse(user_input_id)
+        if not user_input.exists():
             return request.not_found()
 
+        # Prepare values for rendering
         values = {
-            'docs': [user_input],  # Pass as list for compatibility with template looping
-            'share_url': request.httprequest.host_url + 'fna/certification/' + access_token
+            'docs': user_input,
+            'share_url': request.httprequest.host_url + 'fna/certification/' + str(user_input_id)
         }
-        return request.render('survey.certification_report_view', values)
+        return http.request.render('survey.certification_report_view', values)
