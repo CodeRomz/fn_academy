2752db2733f323378f63ca98bc72df1ca3aa89f4
CyberRoms <romualdojrb@yahoo.com>
2024-11-14 10:53:02 +0400

    .

---
+++ controllers/certification_web_view.py
@@ -0,0 +1,14 @@
+from odoo import http
+from odoo.http import request
+
+class SurveyCertificateController(http.Controller):
+    @http.route(['/survey/certification/<int:user_input_id>'], type='http', auth='public', website=True)
+    def view_certificate(self, user_input_id, **kwargs):
+        user_input = request.env['survey.user_input'].sudo().browse(user_input_id)
+        if not user_input.exists():
+            return request.not_found()
+
+        values = {
+            'doc': user_input
+        }
+        return request.render('survey.certification_report_template', values)
