5a6306397b988a22978b3fe80c31d72e8d6f6d20
CyberRoms <romualdojrb@yahoo.com>
2024-11-20 13:04:05 +0400

    .

--- controllers/certification_web_view.py
+++ controllers/certification_web_view.py
@@ -1,8 +1,7 @@
 from odoo import http
-from odoo.http import request, content_disposition
+from odoo.http import request
 
 class SurveyCertificateController(http.Controller):
-
     @http.route(['/fna/certification/<int:user_input_id>'], type='http', auth='public', website=True)
     def view_certificate(self, user_input_id, **kwargs):
         # Fetch the survey response using sudo() to bypass security restrictions
@@ -15,21 +14,4 @@ class SurveyCertificateController(http.Controller):
             'docs': user_input,
             'share_url': request.httprequest.host_url + 'fna/certification/' + str(user_input_id)
         }
-        return request.render('survey.certification_report_view', values)
-
-    @http.route(['/fna/certification/<int:user_input_id>/image'], type='http', auth='public')
-    def certificate_image(self, user_input_id, **kwargs):
-        # Fetch the survey response
-        user_input = request.env['survey.user_input'].sudo().browse(user_input_id)
-        if not user_input.exists() or not user_input.certificate_image:
-            return request.not_found()
-
-        # Serve the certificate_image as a binary file
-        image = user_input.certificate_image
-        return request.make_response(
-            image,
-            headers=[
-                ('Content-Type', 'image/png'),
-                ('Content-Disposition', content_disposition('certificate_image.png')),
-            ]
-        )
+        return http.request.render('survey.certification_report_view', values)
