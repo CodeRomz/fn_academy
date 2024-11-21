b50e95b5165e8eecc01b0e4016b4c06fe26be070
CyberRoms <romualdojrb@yahoo.com>
2024-11-21 15:37:11 +0400

    .

--- controllers/certification_web_view.py
+++ controllers/certification_web_view.py
@@ -21,25 +21,4 @@ class SurveyCertificateController(http.Controller):
             'docs': user_input,
             'share_url': f"{request.httprequest.host_url}fna/certification/{user_input_id}",
         }
-        return request.render('survey.certification_report_view', values)
-
-    # @http.route(['/fna/certification/<int:user_input_id>/image'], type='http', auth='public')
-    # def get_certificate_image(self, user_input_id, **kwargs):
-    #     """
-    #     Serve the certificate image publicly for Open Graph sharing.
-    #     """
-    #     # Fetch the survey.user_input record using sudo()
-    #     user_input = request.env['survey.user_input'].sudo().browse(user_input_id)
-    #     if not user_input.exists() or not user_input.certificate_image:
-    #         # Return a placeholder or 404 if the image is missing
-    #         return request.not_found()
-    #
-    #     # Serve the image as a binary response
-    #     return request.make_response(
-    #         user_input.certificate_image,
-    #         headers=[
-    #             ('Content-Type', 'image/png'),
-    #             ('Content-Disposition', f'inline; filename=certificate_{user_input_id}.png'),
-    #         ]
-    #     )
-
+        return request.render('survey.certification_report_view_web', values)
