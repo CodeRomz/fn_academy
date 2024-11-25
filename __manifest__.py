fac711c57bdb43fa99d6f27dbe0ca8c0074ca3f3
CyberRoms <romualdojrb@yahoo.com>
2024-11-25 16:02:37 +0400

    .

--- __manifest__.py
+++ __manifest__.py
@@ -25,6 +25,7 @@
         'views/survey_user_views.xml',
         'report/custom_survey_template.xml',
         'report/custom_survey_reports.xml',
+        'views/certification_share_view.xml',
 
     ],
     'installable': True,
