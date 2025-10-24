e8e18bc720df838e57fdd5766154f7e9e64f297f
CyberRoms <romualdojrb@yahoo.com>
2025-10-24 12:07:07 +0400

    .

--- __manifest__.py
+++ __manifest__.py
@@ -19,9 +19,9 @@
 
     # always loaded
     'data': [
-        'security/ir.model.access.csv',
         'security/fn_acad_security_rules.xml',
         'security/fn_acad_security_groups.xml',
+        'security/ir.model.access.csv',
         'views/res_users.xml',
         'views/survey_user_views.xml',
         'report/custom_survey_template.xml',
