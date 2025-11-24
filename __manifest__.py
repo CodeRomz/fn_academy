c45aa368737dfd263ad10dde79e24351723d8e7b
CyberRoms <romualdojrb@yahoo.com>
2025-11-24 13:34:25 +0400

    .

--- __manifest__.py
+++ __manifest__.py
@@ -24,7 +24,7 @@
         'security/ir.model.access.csv',
         'views/res_users.xml',
         'views/survey_user_views.xml',
-        'views/elearning_menu_extend.xml',
+        # 'views/elearning_menu_extend.xml',
         'report/custom_survey_template.xml',
         'report/custom_survey_hr_emp_ack_template',
         'report/custom_survey_reports.xml',
